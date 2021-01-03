bbb31c8d8f18ccab3ed83d5a482cc22ffdd97490
mathetes <mathetes@gmail.com>
2021-01-03 18:31:10 +0700

    map data Education & Experience

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -1,37 +1,54 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export const Education = () => (
-    <>
-        <div className="container z-depth-1 my-5 py-5">
+const data = [
+  {
+    id: 1,
+    school: "Karaganda Higher Polytechnic College",
+    period: "1992-1996",
+    link: "https://kvptk.kz/ru",
+    course: "Maintenance and repair of mining electromechanical equipment.",
+  },
+  {
+    id: 2,
+    school: "Novosibirsk Biblical Seminary",
+    period: "2000-2004",
+    link: "https://nbbs.ru",
+    course:
+      "It is the only higher theological institution in the Asiatic part of Russia.",
+  },
+];
 
-            <section>
+const listItems = data.map((item) => (
+  <div className="col-md-6 col-lg-5 col-xl-4" key={item.id}>
+    <h5 className="font-weight-normal border-top border-light pt-4 mb-4">
+      <a className="dark-grey-text" href="{item.link}">
+        {item.school}
+      </a>
+    </h5>
+    <p className="text-muted mb-5 pb-md-3">
+      {item.period} - {item.course} <br />
+      <Link href={item.link}>{item.link}</Link>
+    </p>
+  </div>
+));
 
-                <h3 className="font-weight-bold text-center dark-grey-text pb-2">Education</h3>
-                <hr className="w-header my-4" />
-                <p className="lead text-center text-muted pt-2 mb-5">I seek out new knowledge and actively develop new skills.</p>
+export const Education = (data) => (
+  <>
+    <div className="container z-depth-1 my-5 py-5">
+      <section>
+        <h3 className="font-weight-bold text-center dark-grey-text pb-2">
+          Education
+        </h3>
+        <hr className="w-header my-4" />
+        <p className="lead text-center text-muted pt-2 mb-5">
+          I seek out new knowledge and actively develop new skills.
+        </p>
 
-                <div className="row d-flex justify-content-center">
-
-                    <div className="col-md-6 col-lg-5 col-xl-4">
-                        <h5 className="font-weight-normal border-top border-light pt-4 mb-4"><a className="dark-grey-text" href="#">Karaganda Higher Polytechnic College</a></h5>
-                        <p className="text-muted mb-5 pb-md-3">1992-1996 - Maintenance and repair of mining electromechanical equipment. <br /><Link href="https://kvptk.kz/ru">www.kvptk.kz</Link></p>
-
-                    </div>
-
-                    <div className="col-md-6 col-lg-5 col-xl-4">
-                        <h5 className="font-weight-normal border-top border-secondary pt-4 mb-4"><a className="dark-grey-text"
-                            href="#">Novosibirsk Biblical Seminary</a></h5>
-                        <p className="text-muted mb-5 pb-md-3">2000-2004 - It is the only higher theological institution in the Asiatic part of Russia. <Link href="https://nbbs.ru">www.nbbs.ru</Link></p>
-
-                    </div>
-
-                    <div className="w-100"></div>
-
-                </div>
-            </section>
-
+        <div className="row d-flex justify-content-center">
+          {listItems}
+          <div className="w-100"></div>
         </div>
-
-
-    </>
-)
+      </section>
+    </div>
+  </>
+);
--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,52 +1,63 @@
-import Link from 'next/link';
+import Link from "next/link";
+
+const data = [
+  {
+    id: 1,
+    company: "JSC«NPAF»",
+    period: "11.2016 - Present",
+    position: "Senior System(s) Administrator.",
+    link: "https://nziv.ru",
+  },
+  {
+    id: 2,
+    company: "Trade54 ltd.",
+    period: "03.2015-03.2016",
+    position: "System(s) Administrator.",
+    link: "#",
+  },
+  {
+    id: 3,
+    company: "Private Entrepreneur",
+    period: "2008-2014",
+    position: "Computer repair technician",
+    link: "www.helplinsk.ru",
+  },
+];
+
+const listItems = data.map((item) => (
+  <>
+    <div className="col-md-6 col-xl-4">
+      <h5 className="font-weight-normal">
+        <a className="dark-grey-text" href={item.link}>
+          {item.company}
+        </a>
+      </h5>
+      <p className="small grey-text">{item.period}</p>
+    </div>
+
+    <div className="col-md-6">
+      <p className="text-muted mb-5 pb-2">
+        {item.position}
+        <Link href={item.link}>{item.link}</Link>
+      </p>
+    </div>
+  </>
+));
 
 export const Experience = () => (
-    <>
-        <div className="container z-depth-1 my-5 p-5">
+  <>
+    <div className="container z-depth-1 my-5 p-5">
+      <section>
+        <h3 className="font-weight-bold text-center dark-grey-text pb-2">
+          Experience
+        </h3>
+        <hr className="w-header my-4" />
+        <p className="lead text-center text-muted pt-2 mb-5">
+          I'm specialist working in IT sphere.
+        </p>
 
-            <section>
-                <h3 className="font-weight-bold text-center dark-grey-text pb-2">Experience</h3>
-                <hr className="w-header my-4" />
-                <p className="lead text-center text-muted pt-2 mb-5">I'm specialist working in IT sphere.</p>
-
-                <div className="row d-flex justify-content-center">
-
-                    <div className="col-md-6 col-xl-4">
-                        <h5 className="font-weight-normal"><a className="dark-grey-text" href="#">JSC«NPAF»</a></h5>
-                        <p className="small grey-text">11.2016 - Present</p>
-                    </div>
-
-                    <div className="col-md-6">
-                        <p className="text-muted mb-5 pb-2">Senior System(s) Administrator.<Link href="https://nziv.ru">www.nziv.ru</Link></p>
-
-                    </div>
-
-
-                    <div className="col-md-6 col-xl-4">
-                        <h5 className="font-weight-normal"><a className="dark-grey-text" href="#">Trade54 ltd.</a></h5>
-                        <p className="small grey-text">03.2015-03.2016</p>
-                    </div>
-
-                    <div className="col-md-6">
-                        <p className="text-muted mb-5 pb-2">System(s) Administrator.</p>
-                    </div>
-
-
-                    <div className="col-md-6 col-xl-4">
-                        <h5 className="font-weight-normal"><a className="dark-grey-text" href="#">Private Entrepreneur</a></h5>
-                        <p className="small grey-text">2008-2014</p>
-                    </div>
-
-                    <div className="col-md-6">
-                        <p className="text-muted mb-5 pb-2">Computer repair technician</p>
-                    </div>
-
-                </div>
-
-            </section>
-
-        </div>
-
-    </>
-)
-
+        <div className="row d-flex justify-content-center">{listItems}</div>
+      </section>
+    </div>
+  </>
+);
