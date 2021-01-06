78847a167147ef07bb4ebece338a6c657c4526d7
mathetes <mathetes@gmail.com>
2021-01-06 17:19:38 +0700

    download imagess

---
+++ public/assets/images/index.js
@@ -0,0 +1,15 @@
+import cristall from "./cristall.jpg";
+import unity from "./unity.jpg";
+import tnp2 from "./tnp2.jpg";
+import weather from "./weather.jpg";
+import laximo from "./laximo.jpg";
+import movies from "./movies.jpg";
+
+export default () {
+  cristall,
+  unity,
+  tnp2,
+  weather,
+  laximo,
+  movies,
+};
